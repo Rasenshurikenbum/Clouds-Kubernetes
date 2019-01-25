32861ec421f523c1c16ecf00d9179719f497ecc7
rasenshurikenbum <rasenshurikenbum@wp.pl>
2019-01-25 23:21:28 +0100

    whatever

--- client_app/main.js
+++ client_app/main.js
@@ -58,5 +58,5 @@ app.get('/print', (req, res) => {
 });
 
 http.createServer(app).listen(listeningPort, () => {
-    console.log(`server running on: http://${ipAdress}:${listeningPort}, db_app:${dbAppIP}`);
+    console.log(`server running on: http://${ipAdress}:${listeningPort}`);
 });
